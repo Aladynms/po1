dbddecb6917205c611a83dca1d293732ad20857c
Michał Sala <michal.sala@microsoft.wsei.edu.pl>
2023-04-03 11:44:18 +0200

    11, 12

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,5 +13,32 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int betterThan = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade)
+                    betterThan++;
+            }
+
+            double studentAverageBetter = ((double)betterThan / (double)Students.Count) * 100;
+
+            if (studentAverageBetter < 20) return 'A';
+            else if (studentAverageBetter >= 20 && studentAverageBetter < 40) return 'B';
+            else if (studentAverageBetter >= 40 && studentAverageBetter < 60) return 'C';
+            else if (studentAverageBetter >= 60 && studentAverageBetter < 80) return 'D';
+            else return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) Console.WriteLine("Ranked grading require at least 5 students");
+            else base.CalculateStatistics();
+        }
+
     }
 }
